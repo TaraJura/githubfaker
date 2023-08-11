d291b5f1bc768dbe8242deddbfcd37b1a6a96c92
TaraJura <jirkanovakneda11@seznam.cz>
2023-08-11 17:04:18 +0200

    Initial commit

---
+++ github_activity_faker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set your repository directory and GitHub remote name
+REPO_DIR="$HOME/Github-activity-faker"
+REMOTE_NAME="origin"
+
+# Navigate to the repository
+cd $REPO_DIR
+
+# Generate a random filename
+FILENAME=$(date +%s%N)
+
+# Create the file and add some random content to it
+echo $RANDOM > $FILENAME.txt
+
+# Add the new file to git
+git add $FILENAME.txt
+
+# Commit the changes
+git commit -m "Add random file $FILENAME"
+
+# Push the changes to GitHub
+git push $REMOTE_NAME master
+
